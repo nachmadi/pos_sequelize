77ac9c829cec93243d64a282f2721f9d743d46ff
kholes <cholis.elbazar@gmail.com>
2017-09-14 20:52:42 +0700

    edit deposit

--- router/transaksis.js
+++ router/transaksis.js
@@ -21,36 +21,45 @@ router.post('/',(req,res)=>{
           models.Transaksis.create(transaksi)
           .then(trx=>{
             let TrxId=trx.id;
-            models.Item.findAll()
-            .then(items=>{
-              let c=0;
-              items.forEach(item=>{
-                let temp={
-                  harga:item.harga,
-                  jml_beli:item.jml_beli,
-                  sub_total:item.sub_total,
-                  TransaksisId:TrxId,
-                  BarangsId:item.BarangId
-                }
-                models.Detils.create(temp)
-                .then(insert=>{
-                  models.Barangs.findOne({where:{id:item.BarangId}})
-                  .then(barang=>{
-                    let oldStok=barang.stok;
-                    let newStok=oldStok-item.jml_beli;
-                    let dataStok={
-                      stok:newStok
+            models.Users.findOne({where:{id:req.session.login.userId}})
+            .then(user=>{
+              let oldDeposit=user.deposito;
+              let newDeposito=oldDeposit-req.body.total;
+              objDep={deposito:newDeposito}
+              models.Users.update(objDep,{where:{id:req.session.login.userId}})
+              .then(dep=>{
+                models.Item.findAll()
+                .then(items=>{
+                  let c=0;
+                  items.forEach(item=>{
+                    let temp={
+                      harga:item.harga,
+                      jml_beli:item.jml_beli,
+                      sub_total:item.sub_total,
+                      TransaksisId:TrxId,
+                      BarangsId:item.BarangId
                     }
-                    models.Barangs.update(dataStok,{where:{id:item.BarangId}})
-                    .then(upd=>{
-                      if(c>=items.length){
-                        models.Item.destroy({where: {},truncate: true})
-                        res.redirect('/')
-                      }
+                    models.Detils.create(temp)
+                    .then(insert=>{
+                      models.Barangs.findOne({where:{id:item.BarangId}})
+                      .then(barang=>{
+                        let oldStok=barang.stok;
+                        let newStok=oldStok-item.jml_beli;
+                        let dataStok={
+                          stok:newStok
+                        }
+                        models.Barangs.update(dataStok,{where:{id:item.BarangId}})
+                        .then(upd=>{
+                          if(c>=items.length){
+                            models.Item.destroy({where: {},truncate: true})
+                            res.redirect('/')
+                          }
+                        })
+                      })
                     })
+                    c++;
                   })
-                })
-                c++;
+                })                
               })
             })
           })
